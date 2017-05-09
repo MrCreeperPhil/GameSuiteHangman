60b7b9f43e1ecfdca83198d85e23ce89d9d8423f
Raf-Van-Rompaey <raf.vanrompaey@telenet.be>
2017-05-09 11:59:57 +0200

    doener: raf van rompaey denker:daan lemmens broken ui gemaakt voor speler en figuur. moet nog geloopt worden als er iet fout word ingegeven ....

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -2,15 +2,43 @@ package ui;
 
 import javax.swing.JOptionPane;
 
-import domain.Speler;
 
 public class Launcher {
 
 	public static void main(String[] args) {
-		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
-		Speler speler = new Speler(naam);
-
-		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		
+		
+		int action1 = 0;
+		String action="";
+		while(action.trim().equals("")||action.isEmpty()&& (action!="1" ||action!="2"||action!="0")){
+		    action = JOptionPane.showInputDialog("Wat wil je doen? \n1. speler aanmaken? \n2. figuur tekenen? \n0. exit");
+		}
+		action1= Integer.parseInt(action);
+			switch(action1)
+			{
+			case 0:
+				break;
+			case 1:
+				//nieuwe speller
+				
+				String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
+				//Speler speler = new Speler(naam);
+				//JOptionPane.showMessageDialog(speler.getNaam + " heeft als score:" + speler.getScore);
+				
+				
+				//JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+				break;
+			
+			case 2:
+				
+					String[] choices = { "Punt", "Cirkel", "Lijnstuk", "Driehoek", "Rechthoek" };
+				    String input = (String) JOptionPane.showInputDialog(null, "Choose now...",
+				    		"The Choice of a Lifetime", JOptionPane.QUESTION_MESSAGE, null,
+				    		choices,
+				            choices[0]); 
+				        System.out.println(input);
+				        break;
+			}
 	}
 
 }
