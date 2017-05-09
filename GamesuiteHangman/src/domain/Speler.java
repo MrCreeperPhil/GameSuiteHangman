7ff3c975f47e5ac30809e59b8cc7319c08819fc0
Philip <philip.audenaert@student.ucll.be>
2017-05-09 15:52:37 +0200

    Doener: Philip Audenaert Denker: Bram Kamoen Klasse: Launcher: Nog bezig; Speler: Reformat;

--- GamesuiteHangman/src/domain/Speler.java
+++ GamesuiteHangman/src/domain/Speler.java
@@ -3,51 +3,55 @@ package domain;
 public class Speler {
 	private String naam;
 	private int score = 0;
-	
+
 	public Speler(String naam) {
 		setNaam(naam);
-		
+
 	}
+
 	public String getNaam() {
 		return naam;
 	}
+
 	public void setNaam(String naam) {
-		if(naam == null || naam.trim().isEmpty()){
+		if (naam == null || naam.trim().isEmpty()) {
 			throw new DomainException("Dit mag niet");
-		}
-		else{
+		} else {
 			this.naam = naam;
 		}
 	}
+
 	public int getScore() {
 		return score;
 	}
-	public int addToScore(int pointsToAdd){
+
+	public int addToScore(int pointsToAdd) {
 		int score1 = score;
-		score=score+pointsToAdd;
-		if(score<0){
+		score = score + pointsToAdd;
+		if (score < 0) {
 			score = score1;
 			throw new DomainException("Dit mag niet");
-		}
-		else{
+		} else {
 			setScore(score);
 		}
 		return score;
 	}
+
 	public void setScore(int score) {
 		this.score = score;
 	}
-	public boolean equals(Object object){
-		if(object instanceof Speler){
+
+	public boolean equals(Object object) {
+		if (object instanceof Speler) {
 			Speler speler = (Speler) object;
-			if (this.getNaam().equals(speler.getNaam()) && this.getScore() == speler.getScore()){
+			if (this.getNaam().equals(speler.getNaam()) && this.getScore() == speler.getScore()) {
 				return true;
 			}
 		}
 		return false;
 	}
-	public String toString(){
-		return this.getNaam() +" heeft als score " + this.getScore();
-	}	
-}
 
+	public String toString() {
+		return this.getNaam() + " heeft als score " + this.getScore();
+	}
+}
--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -3,6 +3,7 @@ package ui;
 import javax.swing.JOptionPane;
 import domain.Punt;
 import domain.Cirkel;
+import domain.Speler;
 
 public class Launcher {
 
@@ -12,22 +13,44 @@ public class Launcher {
 		launcher = new Launcher();
 	}
 
+	// launcher blijft loopen zolang run() boolean true is.
 	public boolean run() {
-		int action = Integer.parseInt(
-				JOptionPane.showInputDialog("Wat wil je doen? \n1. speler aanmaken? \n2. figuur tekenen? \n0. exit"));
+		int action = 0;
+		String actionString = JOptionPane
+				.showInputDialog("Wat wil je doen? \n1. speler aanmaken? \n2. figuur tekenen? \n0. exit");
+
+		// Als Cancel knop in inputDialog aangeklikt wordt, wordt de boolean
+		// false en stopt de launcher.
+		if (actionString == null) {
+			return false;
+		}
+		// Als de gebruiker iets anders ingeeft dan een getal, krijgen we een
+		// NumberFormatException.class Deze moet gecaught worden.
+		try {
+			action = Integer.parseInt(actionString);
+		} catch (NumberFormatException e) {
+			System.out.println("Geef een correcte input.");
+			action = 999;
+		}
+
+		// action = Integer.parseInt(actionString);
+
 		switch (action) {
 		case 0:
 			return false;
 		case 1:
-			// nieuwe speller
 
+			// nieuwe speler
 			String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
-			// Speler speler = new Speler(naam);
-			// JOptionPane.showMessageDialog(speler.getNaam + " heeft als
-			// score:" + speler.getScore);
+			try {
+				Speler speler = new Speler(naam);
+				JOptionPane.showMessageDialog(null, speler.getNaam() + " heeft als score: " + speler.getScore());
 
-			// JOptionPane.showMessageDialog(null, "... zal binnekort spelen",
-			// speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(),
+						JOptionPane.INFORMATION_MESSAGE);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
 			break;
 
 		case 2:
@@ -53,6 +76,10 @@ public class Launcher {
 			}
 			break;
 
+		case 999:
+			break;
+		default:
+			break;
 		}
 		return true;
 	}
