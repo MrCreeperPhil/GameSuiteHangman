bf54c12fbbdf3bc1f7d52754a4f5f0f518969c11
Philip <philip.audenaert@student.ucll.be>
2017-05-10 11:16:43 +0200

    Fixed Driehoek en DriehoekTest

--- GamesuiteHangman/src/domain/Driehoek.java
+++ GamesuiteHangman/src/domain/Driehoek.java
@@ -4,24 +4,30 @@ public class Driehoek {
 	private Punt hoekPunt1;
 	private Punt hoekPunt2;
 	private Punt hoekPunt3;
-	
-	public Driehoek(Punt hoekPunt1, Punt hoekPunt2, Punt hoekPunt3){
+
+	public Driehoek(Punt hoekPunt1, Punt hoekPunt2, Punt hoekPunt3) {
 		setHoekPunten(hoekPunt1, hoekPunt2, hoekPunt3);
 	}
-	public void setHoekPunten(Punt hoekPunt1, Punt hoekPunt2, Punt hoekPunt3){
-		if(hoekPunt1 == null || hoekPunt2 == null || hoekPunt3 == null){
+
+	public void setHoekPunten(Punt hoekPunt1, Punt hoekPunt2, Punt hoekPunt3) {
+		if (hoekPunt1 == null || hoekPunt2 == null || hoekPunt3 == null) {
 			throw new DomainException("The points may not be null");
 		}
-		if(hoekPunt1.getX()==hoekPunt2.getX() && hoekPunt1.getY()==hoekPunt2.getY() ||
-				hoekPunt3.getX()==hoekPunt2.getX() && hoekPunt3.getY()==hoekPunt2.getY() || 
-				hoekPunt1.getX()==hoekPunt3.getX() && hoekPunt1.getY()==hoekPunt3.getY()){
+		if (hoekPunt1.getX() == hoekPunt2.getX() && hoekPunt1.getY() == hoekPunt2.getY()
+				|| hoekPunt3.getX() == hoekPunt2.getX() && hoekPunt3.getY() == hoekPunt2.getY()
+				|| hoekPunt1.getX() == hoekPunt3.getX() && hoekPunt1.getY() == hoekPunt3.getY()) {
 			throw new DomainException("All points need to be different");
 		}
-		if((hoekPunt1.getY()-hoekPunt2.getY())/(hoekPunt1.getX()-hoekPunt2.getX())==
-				(hoekPunt2.getY()-hoekPunt3.getY())/(hoekPunt2.getX()-hoekPunt3.getX())){
+		if ((hoekPunt2.getX() - hoekPunt1.getX())
+				* (hoekPunt3.getY() - hoekPunt1.getY()) == (hoekPunt3.getX() - hoekPunt1.getX())
+						* (hoekPunt2.getY() - hoekPunt1.getY())) {
 			throw new DomainException("The points may not be on the same line");
+		} else {
+			this.hoekPunt1 = hoekPunt1;
+			this.hoekPunt2 = hoekPunt2;
+			this.hoekPunt3 = hoekPunt3;
 		}
-		
+
 	}
 
 	public Punt getHoekPunt1() {
@@ -32,24 +38,25 @@ public class Driehoek {
 		return hoekPunt2;
 	}
 
-
 	public Punt getHoekPunt3() {
 		return hoekPunt3;
 	}
 
 	@Override
-	public boolean equals(Object object){
-		if(object instanceof Driehoek){
+	public boolean equals(Object object) {
+		if (object instanceof Driehoek) {
 			Driehoek driehoek = (Driehoek) object;
-			if(this.getHoekPunt1().equals(driehoek.getHoekPunt1()) &&  this.getHoekPunt2().equals(driehoek.getHoekPunt2())  &&  this.getHoekPunt3().equals(driehoek.getHoekPunt3())){
-			return true;
+			if (this.getHoekPunt1().equals(driehoek.getHoekPunt1())
+					&& this.getHoekPunt2().equals(driehoek.getHoekPunt2())
+					&& this.getHoekPunt3().equals(driehoek.getHoekPunt3())) {
+				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	@Override
-	public String toString(){
-		return "Driehoek: hoekpunt1: "+ hoekPunt1 + " - hoekpunt2: " + hoekPunt2 + " - hoekpunt3: " + hoekPunt3;
+	public String toString() {
+		return "Driehoek: hoekpunt1: " + hoekPunt1 + " - hoekpunt2: " + hoekPunt2 + " - hoekpunt3: " + hoekPunt3;
 	}
 }
--- GamesuiteHangman/test/domain/DriehoekTest.java
+++ GamesuiteHangman/test/domain/DriehoekTest.java
@@ -6,15 +6,8 @@ import org.junit.Test;
 
 public class DriehoekTest {
 	private Punt punt1 = new Punt(10, 20);
-	private Punt zelfdeAlsPunt1 = new Punt(10, 20);
-	private Punt verschillendVanPunt1 = new Punt(15, 20);
-	private Punt punt2 = new Punt(20, 40);
-	private Punt zelfdeAlsPunt2 = new Punt(20, 40);
-	private Punt verschillendVanPunt2 = new Punt(40, 20);
-	private Punt punt3 = new Punt(190, 30);
-	private Punt zelfdeAlsPunt3 = new Punt(190, 30);
-	private Punt verschillendVanPunt3 = new Punt(120, 100);
-    private Punt punt4 = new Punt(30,60);
+	private Punt punt2 = new Punt(30, 20);
+	private Punt punt3 = new Punt(20, 30);
 
 	@Test
 	public void Driehoek_moet_DrieHoek_aanmaken_met_gegeven_hoekpunten() {
@@ -24,74 +17,84 @@ public class DriehoekTest {
 		assertEquals(punt2, drieHoek.getHoekPunt2());
 		assertEquals(punt3, drieHoek.getHoekPunt3());
 	}
-	
-	@Test (expected = DomainException.class)
-	public void Driehoek_Moet_exception_gooien_als_hoekpunt1_null()  {
+
+	@Test(expected = DomainException.class)
+	public void Driehoek_Moet_exception_gooien_als_hoekpunt1_null() {
 		new Driehoek(null, punt2, punt3);
 	}
-	
-	@Test (expected = DomainException.class)
-	public void Driehoek_Moet_exception_gooien_als_hoekpunt2_null()  {
+
+	@Test(expected = DomainException.class)
+	public void Driehoek_Moet_exception_gooien_als_hoekpunt2_null() {
 		new Driehoek(punt1, null, punt3);
 	}
-    
-    @Test (expected = DomainException.class)
-    public void Driehoek_moet_Exception_gooien_als_Punt1_Punt2_en_Punt3_op_een_lijn_liggen(){
-        new Driehoek(punt1, punt2, punt4);
-    }
-	
-	@Test (expected = DomainException.class)
-	public void DrieHoek_Moet_exception_gooien_als_hoekpunt3_null()  {
+
+	@Test(expected = DomainException.class)
+	public void Driehoek_moet_Exception_gooien_als_Punt1_Punt2_en_Punt3_op_een_lijn_liggen() {
+		Punt punt4 = new Punt(40, 20);
+		new Driehoek(punt1, punt2, punt4);
+	}
+
+	@Test(expected = DomainException.class)
+	public void DrieHoek_Moet_exception_gooien_als_hoekpunt3_null() {
 		new Driehoek(punt1, punt2, null);
 	}
-    @Test (expected = DomainException.class)
-    public void Driehoek_moet_exception_gooien_als_hoekpunt1_en_hoekpunt2_gelijk(){
-        new Driehoek (punt1, zelfdeAlsPunt1, punt3);
-    }
-    @Test (expected = DomainException.class)
-    public void Driehoek_moet_exception_gooien_als_hoekpunt1_en_hoekpunt3_gelijk(){
-        new Driehoek (punt1, punt2, zelfdeAlsPunt1);
-    }
-    @Test (expected = DomainException.class)
-    public void Driehoek_moet_exception_gooien_als_hoekpunt2_en_hoekpunt3_gelijk2(){
-        new Driehoek (punt1, punt2, zelfdeAlsPunt2);
-    }
-    @Test (expected = DomainException.class)
-    public void Driehoek_moet_exception_gooien_als_hoekpunt2_en_hoekpunt1_gelijk(){
-        new Driehoek (zelfdeAlsPunt2, punt2, punt3);
-    }
-    @Test (expected = DomainException.class)
-    public void Driehoek_moet_exception_gooien_als_hoekpunt3_en_hoekpunt2_gelijk(){
-        new Driehoek (punt1, zelfdeAlsPunt3, punt3);
-    }
-    @Test (expected = DomainException.class)
-    public void Driehoek_moet_exception_gooien_als_hoekpunt3_en_hoekpunt1_gelijk(){
-        new Driehoek (zelfdeAlsPunt3, punt2, punt3);
-    }
-	
+
+	@Test(expected = DomainException.class)
+	public void Driehoek_moet_exception_gooien_als_hoekpunt1_en_hoekpunt2_gelijk() {
+		new Driehoek(punt1, punt1, punt3);
+	}
+
+	@Test(expected = DomainException.class)
+	public void Driehoek_moet_exception_gooien_als_hoekpunt1_en_hoekpunt3_gelijk() {
+		new Driehoek(punt1, punt2, punt1);
+	}
+
+	@Test(expected = DomainException.class)
+	public void Driehoek_moet_exception_gooien_als_hoekpunt2_en_hoekpunt3_gelijk2() {
+		new Driehoek(punt1, punt2, punt2);
+	}
+
+	@Test(expected = DomainException.class)
+	public void Driehoek_moet_exception_gooien_als_hoekpunt2_en_hoekpunt1_gelijk() {
+		new Driehoek(punt2, punt2, punt3);
+	}
+
+	@Test(expected = DomainException.class)
+	public void Driehoek_moet_exception_gooien_als_hoekpunt3_en_hoekpunt2_gelijk() {
+		new Driehoek(punt1, punt3, punt3);
+	}
+
+	@Test(expected = DomainException.class)
+	public void Driehoek_moet_exception_gooien_als_hoekpunt3_en_hoekpunt1_gelijk() {
+		new Driehoek(punt3, punt2, punt3);
+	}
+
 	@Test
-	public void equals_moet_false_teruggeven_als_hoekPunt1_verschillend(){
+	public void equals_moet_false_teruggeven_als_hoekPunt1_verschillend() {
+		Punt verschillendVanPunt1 = new Punt(15, 20);
 		Driehoek drieHoek = new Driehoek(punt1, punt2, punt3);
-		Driehoek andereDriehoek = new Driehoek(verschillendVanPunt1, zelfdeAlsPunt2, zelfdeAlsPunt3);
+		Driehoek andereDriehoek = new Driehoek(verschillendVanPunt1, punt2, punt3);
 		assertFalse(drieHoek.equals(andereDriehoek));
 	}
-	
+
 	@Test
-	public void equals_moet_false_teruggeven_als_hoekPunt2_verschillend(){
+	public void equals_moet_false_teruggeven_als_hoekPunt2_verschillend() {
+		Punt verschillendVanPunt2 = new Punt(50, 30);
 		Driehoek drieHoek = new Driehoek(punt1, punt2, punt3);
-		Driehoek andereDriehoek = new Driehoek(zelfdeAlsPunt1, verschillendVanPunt2, zelfdeAlsPunt3);
+		Driehoek andereDriehoek = new Driehoek(punt1, verschillendVanPunt2, punt3);
 		assertFalse(drieHoek.equals(andereDriehoek));
 	}
-	
+
 	@Test
-	public void equals_moet_false_teruggeven_als_hoekPunt3_verschillend(){
+	public void equals_moet_false_teruggeven_als_hoekPunt3_verschillend() {
+		Punt verschillendVanPunt3 = new Punt(30, 40);
 		Driehoek drieHoek = new Driehoek(punt1, punt2, punt3);
-		Driehoek andereDriehoek = new Driehoek(zelfdeAlsPunt1, zelfdeAlsPunt2, verschillendVanPunt3);
+		Driehoek andereDriehoek = new Driehoek(punt1, punt2, verschillendVanPunt3);
 		assertFalse(drieHoek.equals(andereDriehoek));
 	}
-	
+
 	@Test
-	public void equals_moet_false_teruggeven_als_parameter_null(){
+	public void equals_moet_false_teruggeven_als_parameter_null() {
 		Driehoek drieHoek = new Driehoek(punt1, punt2, punt3);
 		assertFalse(drieHoek.equals(null));
 	}
