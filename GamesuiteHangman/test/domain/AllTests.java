111397767e2388c742d6269ef4dce0fb453afb59
Philip <philip.audenaert@student.ucll.be>
2017-05-11 14:08:09 +0200

    Testsuite update

--- GamesuiteHangman/test/domain/AllTests.java
+++ GamesuiteHangman/test/domain/AllTests.java
@@ -5,7 +5,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PuntTest.class, SpelerTest.class, CirkelTest.class, RechthoekTest.class, LijnStukTest.class })
+@SuiteClasses({ CirkelTest.class, DriehoekTest.class, LijnStukTest.class, OmhullendeTest.class, PuntTest.class,
+		RechthoekTest.class, SpelerTest.class, TekeningTest.class })
+
 public class AllTests {
 
 }
